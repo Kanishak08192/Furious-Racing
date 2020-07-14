e9214669f1aa2e366fd73ab7710e7f61717dc8b9
Kanishak08192 <60224870+Kanishak08192@users.noreply.github.com>
2020-07-14 14:46:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,18 +1,36 @@
-var form;
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-background_img = loadImage("images/back (2).jpg")
+  track = loadImage("../images/track.jpg");
+  background_img = loadImage("../images/back (2).jpg");
+  car1_img = loadImage("../images/car1.png");
+  car2_img = loadImage("../images/car2.png");
+  car3_img = loadImage("../images/car3.png");
+  car4_img = loadImage("../images/car4.png");
+  ground = loadImage("../images/ground.png");
 }
 
-function setup() {
-  createCanvas(500,480);
-  form = new Form (displayWidth,displayHeight,10,10)
+function setup(){
+  canvas = createCanvas(displayWidth + 30, displayHeight +70);
+  background(background_img);
+ form = new Form();
+  database = firebase.database();
+
 }
 
-function draw() {
-  background(background_img); 
-  image(background_img,displayWidth,displayHeight,10,10);
 
+function draw(){
   form.display();
 }
-
